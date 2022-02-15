a3636aa9b2028ae8b041d1a40d1b56d22c947641
AdilNehal22 <adilnehal80@gmail.com>
2022-02-15 17:12:17 +0500

    Little tweaks

--- hardhat.config.js
+++ hardhat.config.js
@@ -12,6 +12,20 @@ module.exports = {
       url: "https://data-seed-prebsc-1-s1.binance.org:8545",
       chainId: 97,
       accounts: [process.env.PRIVATE_KEY]
-    }
+    },
+    fantomtest: {
+      url: "https://rpc.testnet.fantom.network",
+      accounts: [process.env.PRIVATE_KEY],
+      chainId: 4002,
+      live: false,
+      saveDeployments: true,
+      gasMultiplier: 2,
+    },
+    avalancheTest: {
+      url: 'https://api.avax-test.network/ext/bc/C/rpc',
+      gasPrice: 225000000000,
+      chainId: 43113,
+      accounts: [process.env.PRIVATE_KEY]
+    },
   }
 };
